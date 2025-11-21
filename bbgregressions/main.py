1415f2af9a2e71c87dd14c5654cbf6617708873c
rblancomi <raquel.blanco@irbbarcelona.org>
2025-11-21 18:07:53 +0100

    chore: refactor main to use click and daiquiri logger (taken from omega package)

--- bbgregressions/main.py
+++ bbgregressions/main.py
@@ -1,11 +1,70 @@
-import os 
+import click
+import daiquiri
 
-os.makedirs(output_dir, exist_ok = True)
+# from omega import __logger_name__, __version__
 
-# read data
-os.makedirs(os.path.join(output_dir, "input"), exist_ok = True)
-readers()
+from bbgregressions.create_input.main   import main as create_input_main
+from bbgregressions.regressions.main    import main as regressions_main
+from bbgregressions.plot.main           import main as plot_main
 
-# regressions
+# from omega.src.globals              import DATE, setup_logging_decorator, startup_message
 
-# plot
+logger = daiquiri.getLogger(__logger_name__)
+
+
+# TODO: os.makedirs(output_dir, exist_ok = True) somewhere
+
+@click.group(context_settings={'help_option_names': ['-h', '--help']})
+@click.version_option(__version__)
+def bbgregressions():
+    """bbgregressions: software for customized regression models"""
+    pass
+
+
+@bbgregressions.command(context_settings=dict(help_option_names=['-h', '--help']),
+                help="Build input tables")
+@click.option('--option1-example', type=click.Choice(['', '', '']), default = 'hg38', help='')
+@click.option('--option2-example', type=click.Path(exists=True), help='')
+@click.option('--option2-example', type=click.STRING, default = None, help='')
+@setup_logging_decorator
+def create_input(args):
+    """Build formatted input tables to run regressions"""
+    startup_message(__version__, "Initializing input formatting...")
+
+    logger.info("example message")
+    logger.info(f"example message: {laa}")
+
+    create_input_main(args)
+
+
+@omega.command(context_settings=dict(help_option_names=['-h', '--help']),
+                        help="Run regressions")
+@click.option('--option1-example', type=click.Choice(['', '', '']), default = 'hg38', help='')
+@click.option('--option2-example', type=click.Path(exists=True), help='')
+@click.option('--option2-example', type=click.STRING, default = None, help='')
+@setup_logging_decorator
+def regressions(args):
+    """message"""
+    startup_message(__version__, "Running regressions...")
+
+    logger.info("example message")
+    logger.info(f"example message: {laa}")
+    regressions_main(args)
+
+
+@omega.command(context_settings=dict(help_option_names=['-h', '--help']),
+                        help="Plot results")
+@click.option('--option1-example', type=click.Choice(['', '', '']), default = 'hg38', help='')
+@click.option('--option2-example', type=click.Path(exists=True), help='')
+@click.option('--option2-example', type=click.STRING, default = None, help='')
+@setup_logging_decorator
+def plot(args):
+    startup_message(__version__, "Plotting...")
+
+    logger.info("example message")
+    logger.info(f"example message: {laa}")
+    mutabilities_main(args)
+
+
+if __name__ == "__main__":
+    bbgregressions() 
