1d408f8d9a1ef37d6c55e2230c5f084be6caf308
rblancomi <raquel.blanco@irbbarcelona.org>
2026-03-04 05:17:46 +0100

    add: bypass Singular Matrix error in models

--- src/regressions/models.py
+++ src/regressions/models.py
@@ -3,6 +3,7 @@ from itertools import product
 import daiquiri
 import pandas as pd
 import statsmodels.formula.api as smf
+import numpy as np
 
 from src import __logger_name__
 from src.regressions.utils import add_intercept, correct_pvals, fill_storage
@@ -31,7 +32,8 @@ MODELS = {"linear": linear, "linear-mixed-effects": linear_me}
 
 
 def main(data: pd.DataFrame, results: dict, elements: list, predictors: list, config: dict, mode=str) -> dict:
-    """ """
+    """ 
+    """
 
     if mode == "uni":
         terms = product(elements, predictors)
@@ -43,9 +45,18 @@ def main(data: pd.DataFrame, results: dict, elements: list, predictors: list, co
 
         formula = f"{element} ~ {predictors}{intercept}"
         logger.debug(f"Running: {formula}")
-        model = MODELS[config["model"]]
-        model_res = model(data, formula, config)
-        results = fill_storage(results, model_res, element, predictors, intercept)
+        
+        try:
+            model = MODELS[config["model"]]
+            model_res = model(data, formula, config)
+            results = fill_storage(results, model_res, element, predictors, intercept)
+        except np.linalg.LinAlgError as e:
+            if "Singular matrix" in str(e):
+                logger.warning(f"Singular matrix encountered for formula: {formula}. Skipping this model.")
+            else:
+                raise
+        except Exception as e:
+            logger.error(f"Unexpected error for formula {formula}: {str(e)}")
 
     if config["correct_pvals"]:
         results = correct_pvals(results)
