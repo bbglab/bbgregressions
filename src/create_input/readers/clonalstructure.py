ae7775f46d00e921958357ebbef26c1b6c9f8072
FerriolCalvet <ferriolcalvet@gmail.com>
2026-08-21 12:27:06 +0200

    add computation of depth per sample as ALLGENES

--- src/create_input/readers/clonalstructure.py
+++ src/create_input/readers/clonalstructure.py
@@ -131,12 +131,17 @@ def depths(config: dict, output_dir: str) -> pd.DataFrame:
     # load data
     data = pd.read_csv(config["file"], sep="\t")
     data = data.rename({"GENE": "element", "SAMPLE_ID": "sample", "MEAN_GENE_DEPTH": "depth"}, axis=1)
+    sample_depth = data.groupby("sample").agg({"GENE_SEQ": "sum", "GENE_SIZE": "sum"}).reset_index()
+    sample_depth["depth"] = sample_depth["GENE_SEQ"] / sample_depth["GENE_SIZE"]
+    sample_depth["element"] = 'ALLGENES'
+    data = pd.concat([data, sample_depth[["element", "sample", "depth"]]], axis=0)
     data["depth"] = data["depth"] / 1000
+
 
     # read filters
     if not config["elements"]:
         # elements = [elem for elem in data["element"].unique() if "--" not in elem] # removes sub-genic regions
-        elements = [f"{elem}" for elem in elements]
+        elements = [f"{elem}" for elem in data["element"].unique()]
     else:
         elements = config["elements"]
     samples = data["sample"].unique() if not config["samples"] else config["samples"]
